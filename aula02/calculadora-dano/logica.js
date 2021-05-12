954e0c340d241d3c4278436f5cc25dabe1da6243
Paulo Salvatore <salvatoregames@gmail.com>
2021-05-12 16:46:58 -0300

    Code review

--- aula02/calculadora-dano/logica.js
+++ aula02/calculadora-dano/logica.js
@@ -1,20 +1,21 @@
-let vidaPorPersonagem = {
+const vidaPorPersonagem = {
     'globin': 8,
     'orc': 12,
-    'feiticeira': 18
-}
+    'feiticeira': 18,
+};
 
-let danoPorArma = {
+const danoPorArma = {
     'soco': 2,
     'arco': 5,
-    'espada': 10
-}
+    'espada': 10,
+};
 
 let personagemSelecionado;
 let armaSelecionada;
 
 function iniciar() {
-    var elementos = document.getElementsByClassName('elemento');
+    const elementos = document.getElementsByClassName('elemento');
+
     for (let i = 0; i < elementos.length; i++) {
         elementos[i].addEventListener('click', marcarElementoSelecionado);
     }
@@ -25,7 +26,7 @@ function iniciar() {
 function marcarElementoSelecionado(evento) {
     const elementoSelecionado = evento.target.parentElement;
     const idElementoSelecionado = elementoSelecionado.getAttribute('id');
-    
+
     if (elementoSelecionado.classList.contains('personagem')) {
         personagemSelecionado = idElementoSelecionado;
         limparElementosSelecionados('personagem');
@@ -33,7 +34,7 @@ function marcarElementoSelecionado(evento) {
         armaSelecionada = idElementoSelecionado;
         limparElementosSelecionados('arma');
     }
-    
+
     elementoSelecionado.classList.add('selecionado');
 }
 
@@ -43,23 +44,25 @@ function calcularDano() {
         return;
     }
 
-    let danoDados = rolarOsDados();
-    let danoArma = danoPorArma[armaSelecionada];
-    let danoTotal = danoDados + danoArma;
-    let vidaPersonagem = vidaPorPersonagem[personagemSelecionado];
+    const danoDados = rolarOsDados();
+    const danoArma = danoPorArma[armaSelecionada];
+    const danoTotal = danoDados + danoArma;
+    const vidaPersonagem = vidaPorPersonagem[personagemSelecionado];
 
     let resultado = 'Dano: ' + danoTotal + '!';
+
     if (danoTotal >= vidaPersonagem) {
         resultado += ' Parabéns você matou ' + personagemSelecionado;
     } else {
-        resultado += ' Putz, não foi dessa vez, tente novamente!'
+        resultado += ' Putz, não foi dessa vez, tente novamente!';
     }
 
     document.getElementById('dano').innerHTML = resultado;
 }
 
 function limparElementosSelecionados(tipo) {
-    var elementos = document.getElementsByClassName('elemento');
+    const elementos = document.getElementsByClassName('elemento');
+
     for (let i = 0; i < elementos.length; i++) {
         if (elementos[i].classList.contains(tipo)) {
             elementos[i].classList.remove('selecionado');
@@ -68,7 +71,8 @@ function limparElementosSelecionados(tipo) {
 }
 
 function rolarOsDados() {
-    min = Math.ceil(1);
-    max = Math.floor(10);
+    const min = Math.ceil(1);
+    const max = Math.floor(10);
+
     return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
+}
